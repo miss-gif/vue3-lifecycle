67035295f78887ea4cfff5f54eee5091d78e58ad
miss-gif <ddc.akari@example.com>
2022-07-16 21:35:19 +0900

    init

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <h1 @click="increase">
+    {{ count }}
+  </h1>
+  <div v-if="count > 4">4보다 큽니다!</div>
+  <ul>
+    <Fruit v-for="fruit in fruits" :key="fruit" :name="fruit">
+      {{ fruit }}
+    </Fruit>
+  </ul>
+</template>
+
+<script>
+import Fruit from "~/components/Fruit";
+export default {
+  components: {
+    Fruit,
+  },
+  data() {
+    return {
+      count: 0,
+      fruits: ["사과", "바나나", "체리"],
+    };
+  },
+  methods: {
+    increase() {
+      this.count += 1;
+    },
+  },
+};
+</script>
