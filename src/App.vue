ce03f70e915081bb578e5240abb5c1237bd4eb0b
miss <ddc.akari@gmail.com>
2022-07-18 07:01:37 +0900

    조건부 렌더링

--- src/App.vue
+++ src/App.vue
@@ -1,41 +1,21 @@
 <template>
-  <h1 v-bind:class="{ active: isActive }" @click="activate">
-    Hello?{{ isActive }}
-  </h1>
-
-  <h1 :style="[fontStyle, backgroundStyle]" @click="changeStyle">Hello?</h1>
+  <button @click="handler">Click me!</button>
+  <h1 v-if="isShow">Hello?!</h1>
 </template>
 
 <script>
 export default {
   data() {
     return {
-      isActive: false,
-      fontStyle: {
-        color: "orange",
-        fontSize: "30px",
-      },
-      backgroundStyle: {
-        backgroundColor: "black",
-      },
+      isShow: false,
+      count: 0,
     };
   },
   methods: {
-    activate() {
-      this.isActive = true;
-    },
-    changeStyle() {
-      this.fontStyle.color = "red";
-      this.fontStyle.fontSize = "50px";
+    handler() {
+      this.isShow = !this.isShow;
+      this.count += 1;
     },
   },
 };
 </script>
-
-<style scoped>
-.active {
-  color: rebeccapurple;
-  font-weight: 900;
-  font-size: 20px;
-}
-</style>
