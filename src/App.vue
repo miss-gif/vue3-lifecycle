df584d5fdbbc1acfb9858e8b2ee00a0e018231ad
miss <ddc.akari@gmail.com>
2022-07-17 10:35:30 +0900

    인스턴스와 라이프사이클

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,31 @@
 <template>
-  <!-- Remove & use example component! -->
-  <HelloWorld
-    :msg="message"
-    @click="handler" />
-  <HelloWorld msg="Good" />
+  <h1>{{ count }}</h1>
 </template>
 
 <script>
-import HelloWorld from '~/components/HelloWorld'
-
 export default {
-  components: {
-    HelloWorld
-  },
   data() {
     return {
-      message: 'Hello Vue!'
-    }
+      count: 2,
+    };
   },
-  methods: {
-    handler() {
-      console.log(this.message)
-      this.message += '!'
-    }
-  }
-}
+  // 컴포넌트 생성 직전, 활용도 낮음
+  beforeCreate() {
+    console.log("before Create", this.count);
+  },
+  // 컴포넌트 생성 직후
+  created() {
+    console.log("Created!", this.count);
+  },
+  // html 연결 전, 활용도 낮음
+  beforeMount() {
+    console.log("before Mount");
+    console.log(document.querySelector("h1"))
+  },
+  // html 연결 후
+  mounted() {
+    console.log("mounted!");
+    console.log(document.querySelector("h1"))
+  },
+};
 </script>
