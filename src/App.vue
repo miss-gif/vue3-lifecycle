9205bdc3ea2d55bb09fc9b0c6ac1f5991224ab4a
miss <ddc.akari@gmail.com>
2022-07-18 06:17:42 +0900

    watch

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,5 @@
 <template>
-  <button @click="add">add</button>
-  <h1>{{ reversedMsg }}</h1>
-  <h1>{{ reversedMsg }}</h1>
+  <h1 @click="changeMsg">{{ msg }}</h1>
   <h1>{{ reversedMsg }}</h1>
 </template>
 
@@ -9,43 +7,30 @@
 export default {
   data() {
     return {
-      // Getter, Setter
-      msg: "Hello Computed!!!",
+      msg: "Hello???",
     };
   },
   computed: {
-    // Getter, 값을 얻어내는 용도
-    // reversedMsg() {
-    //   return this.msg.split("").reverse().join("");
-    // }, // 계산된 데이터, 캐싱, 읽기 전용
-
-    // Getter, Setter
-    reversedMsg: {
-      // 값을 얻어 냄
-      get() {
-        return this.msg.split("").reverse().join("");
-      },
-      // 값을 할당 함
-      set(v) {
-        this.msg = v;
-      },
+    reversedMsg() {
+      return this.msg.split("").reverse().join("");
+    },
+  },
+  // 변경되는 사항을 감지
+  watch: {
+    // msg() {
+    //   console.log("msg:", this.msg);
+    // },
+    msg(value) {
+      console.log("msg:", value);
+    },
+    reversedMsg() {
+      console.log("reversedMsg:", this.reversedMsg);
     },
   },
   methods: {
-    add() {
-      this.reversedMsg += "?!";
-      // this.msg += "?!";
+    changeMsg() {
+      this.msg = "Good!!!!!!!";
     },
   },
 };
 </script>
-
-<style scoped>
-h1 {
-  font-size: 50px;
-}
-
-button {
-  margin: 50px;
-}
-</style>
