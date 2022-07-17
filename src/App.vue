cc2a35e1637bc9bdc778bbd09c311f0edb79324d
miss <ddc.akari@gmail.com>
2022-07-18 06:07:40 +0900

    Getter, Setter

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,40 @@
 <template>
+  <button @click="add">add</button>
   <h1>{{ reversedMsg }}</h1>
-  <h1>{{ reverseMsg() }}</h1>
+  <h1>{{ reversedMsg }}</h1>
+  <h1>{{ reversedMsg }}</h1>
 </template>
 
 <script>
 export default {
   data() {
     return {
+      // Getter, Setter
       msg: "Hello Computed!!!",
     };
   },
   computed: {
-    reversedMsg() {
-      return this.msg.split("").reverse().join("");
-    }, // 계산된 데이터, 캐싱
+    // Getter, 값을 얻어내는 용도
+    // reversedMsg() {
+    //   return this.msg.split("").reverse().join("");
+    // }, // 계산된 데이터, 캐싱, 읽기 전용
+
+    // Getter, Setter
+    reversedMsg: {
+      // 값을 얻어 냄
+      get() {
+        return this.msg.split("").reverse().join("");
+      },
+      // 값을 할당 함
+      set(v) {
+        this.msg = v;
+      },
+    },
   },
   methods: {
-    reverseMsg() {
-      return this.msg.split("").reverse().join("");
+    add() {
+      this.reversedMsg += "?!";
+      // this.msg += "?!";
     },
   },
 };
@@ -25,6 +42,10 @@ export default {
 
 <style scoped>
 h1 {
-  font-size: 100px;
+  font-size: 50px;
+}
+
+button {
+  margin: 50px;
 }
 </style>
